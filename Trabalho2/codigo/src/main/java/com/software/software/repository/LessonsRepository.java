59b44138c4f2e45af3d5249ec266bce96d5e808b
PhilZeraNTC <pedro.henrique.vogt@gmail.com>
2025-10-27 12:04:19 -0300

    upload diagramaRepository

--- Trabalho2/codigo/src/main/java/com/software/software/repository/LessonsRepository.java
+++ Trabalho2/codigo/src/main/java/com/software/software/repository/LessonsRepository.java
@@ -4,5 +4,5 @@ import com.software.software.models.Lessons;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LessonsRepository extends JpaRepository<Lessons, Long> {
-
+    
 }
